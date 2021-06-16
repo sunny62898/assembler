56d3d3fe0ed1d57b57c544d6922be23a18630ba6
sunny62898 <85967575+sunny62898@users.noreply.github.com>
2021-06-17 05:49:02 +0800

    更改檔案並讀檔
    變更檔案為assembler.cpp
    寫了讀檔部分
    opcode.txt讀檔寫完了
    source.txt讀檔案內容有問題
    (當遇到第三格為空時,下個index就會直接跳過name部分)

--- assembler.c
+++ assembler.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct opcode{
+	String operate[];
+	int operateCode;
+};
+
 int main(){
 	printf("Hello World!\n");
 	system("PAUSE");
---
+++ assembler.cpp
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct opcode{
+	char operate[16] = {};
+	char operateCode[16] = {};
+};
+
+struct source{
+	int location;
+	char name [16] = {};
+	char type [16] = {};
+	char content [16] = {};
+};
+
+int main(){
+	
+	/*read file begain*/
+	FILE *fpOp;
+	fpOp = fopen("opcode.txt","r");   //開檔
+	int opSize = 1;
+	char ch;
+	
+	/*算opcode有幾個 */
+	while(fscanf(fpOp,"%c",&ch) != -1){  
+		if(ch == '\n'){
+			opSize++;
+		}
+	
+	} 
+	fclose(fpOp);  //關檔 
+	/*算完opcodeSize*/
+	struct opcode opcode[opSize];
+	
+	/*read opcode.txt內容*/
+	//FILE *fpOp;
+	fpOp = fopen("opcode.txt","r");   //開檔
+	int index = 0;
+	while(fscanf(fpOp,"%c",&ch) != -1){
+		int i = 0;
+		if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){  //operation
+			opcode[index].operate[i] = ch;
+			i++;
+			fscanf(fpOp,"%c",&ch);
+			while(ch != ' '){
+				opcode[index].operate[i] = ch;
+				i++;
+				fscanf(fpOp,"%c",&ch);
+			}
+		}
+		fscanf(fpOp,"%c",&ch);
+		i = 0;
+		while(ch != '\n'){    //opcode
+			opcode[index].operateCode[i] = ch;
+			if(fscanf(fpOp,"%c",&ch) == -1){
+				break;
+			}
+			i++;
+		}	
+		
+		
+		if(ch == '\n'){
+			index++;
+		}
+	}
+	fclose(fpOp);  //關檔 
+	/*end of read opcode.txt內容*/
+	
+	
+	FILE *fpSource;
+	fpSource = fopen("source.txt","r");   //開檔
+	int SSize = 1;
+	
+	/*計算source.txt的大小*/
+	while(fscanf(fpSource,"%c",&ch) != -1){  
+		if(ch == '\n'){
+			SSize++;
+		}
+	
+	} 
+	fclose(fpSource);  //關檔 
+	/*算完SourceSize*/
+	struct source source[SSize];
+	
+	fpSource = fopen("source.txt","r");   //開檔
+	index = 0;
+	/*read source.txt內容*/
+	while(fscanf(fpSource,"%c",&ch) != -1){
+		printf("%d , %c\n",index,ch);
+		int i = 0;
+		while(ch != '\t'){ //name 
+			//printf("%c",ch);
+			source[index].name[i] = ch;
+			i++;
+			fscanf(fpSource,"%c",&ch);
+		}
+		fscanf(fpSource,"%c",&ch);
+		i = 0;
+		while(ch != '\t'){ //type
+			//printf("%c",ch);
+			source[index].type[i] = ch;
+			i++;
+			fscanf(fpSource,"%c",&ch);
+		}
+		//printf("\n",ch);
+		fscanf(fpSource,"%c",&ch);
+		i = 0;
+		while(ch != '\n'){ //content
+			//printf("%c",ch);
+			source[index].content[i] = ch;
+			i++;
+			if(fscanf(fpSource,"%c",&ch) == -1){
+				break;
+			}
+		}
+		printf("%s\n",source[index].type);
+		index++;
+			
+	}
+	fclose(fpSource);  //關檔 
+	
+	printf("%d\n",SSize);
+	
+	system("PAUSE");
+	return 0;
+} 
